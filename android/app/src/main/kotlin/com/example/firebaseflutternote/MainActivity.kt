e14ad83abf76f8b8c22eb64b5bff78c8d774a20a
Remrem1645 <dyan@cpp.edu>
2022-07-20 18:00:41 -0700

    startin

---
+++ android/app/src/main/kotlin/com/example/firebaseflutternote/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebaseflutternote
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/style/app_style.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppStyle{
+  static Color mainColor = const Color(0xFF000633);
+  static Color accentColor = const Color(0xFF0065FF);
+  static Color bgColor = const Color(0xFFe2e2ff);
+
+  //Cards
+  static List<Color> cardsColor = [
+    Colors.white,
+    Colors.red.shade100,
+    Colors.pink.shade100,
+    Colors.orange.shade100,
+    Colors.yellow.shade100,
+    Colors.green.shade100,
+    Colors.blue.shade100,
+    Colors.blueGrey.shade100,
+    Colors.lightBlue.shade100,
+  ];
+
+  //text style
+
+  static TextStyle mainTitle = 
+    GoogleFonts.roboto(fontSize: 18.0, fontWeight: FontWeight.bold);
+
+  static TextStyle mainContent = 
+    GoogleFonts.nunito(fontSize: 16.0, fontWeight: FontWeight.normal);
+
+  static TextStyle dateTitle = 
+    GoogleFonts.roboto(fontSize: 13.0, fontWeight: FontWeight.w500);
+}
