beba0b88cd6d367152661a598011f27d429aec07
Remrem1645 <dyan@cpp.edu>
2022-07-21 01:23:14 -0700

    changed firebase's data set, updated code to reflect that

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebaseflutternote/style/app_style.dart';
+import 'package:firebaseflutternote/widgets/note_card.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -21,44 +22,66 @@ class _HomeScreenState extends State<HomeScreen> {
       
       appBar: AppBar(
         elevation: 0.0,
-        title: const Text("AnyWhere Notes"),
+        title: const Text(
+          "AnyWhere Notes", 
+          style: TextStyle(
+            color: Colors.black87,
+            fontSize: 23,
+            fontWeight: FontWeight.bold,
+          )
+        ),
         centerTitle: true,
         backgroundColor: AppStyle.mainColor,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Your recent Notes",
-            style: GoogleFonts.roboto(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22),
-          ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          StreamBuilder<QuerySnapshot>(
-            stream: FirebaseFirestore.instance.collection("notes").snapshots(),
-            builder: (context, AsyncSnapshot snapshot){
-              if(snapshot.connectionState == ConnectionState.waiting){
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-              if(snapshot.hasData){
-                return GridView(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2));
-              }
-              return Text("There is nothing yet...", style: GoogleFonts.nunito(color: Colors.white) ,);
-            },
-          ),
-          MaterialButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            color: Colors.deepOrange[200],
-            child: const Text('SignOut'),
-          )
-        ],
+
+      
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 10,),
+            Text(
+              "  Your recent Notes",
+              textAlign: TextAlign.left,
+              style: GoogleFonts.roboto(
+                  color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 20,),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance.collection("notes").doc("6ytgdeDEYAkv0cyW1fIy").collection('UserNotes').snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot){
+                  if(snapshot.connectionState == ConnectionState.waiting){
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if(snapshot.hasData){
+                    return GridView(
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2),
+                      children: snapshot.data!.docs.map((note) => noteCard(() {}, note) ).toList(),
+                    );
+                  }
+                  return Text("There is nothing yet...", style: GoogleFonts.nunito(color: Colors.black87) ,);
+                },
+              ),
+            ),
+
+            MaterialButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              color: Colors.deepOrange[200],
+              child: const Text('SignOut'),
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/note_card.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebaseflutternote/style/app_style.dart';
+import 'package:flutter/material.dart';
+
+Widget noteCard(Function()? onTap, QueryDocumentSnapshot doc) {
+  return InkWell(
+    onTap: onTap,
+    child: Container(
+      padding: const EdgeInsets.all(8.0),
+      margin: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: AppStyle.cardsColor[doc['color_id']],
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(doc['note_title'], style: AppStyle.mainTitle,),  
+          const SizedBox(height: 3.0,),
+          Text(doc['creation_date'], style: AppStyle.dateTitle,),
+          const SizedBox(height: 5.0,),
+          Text(doc['note_content'], style: AppStyle.mainContent, overflow: TextOverflow.ellipsis,)
+        ]
+      )
+    ),
+  );
+}
